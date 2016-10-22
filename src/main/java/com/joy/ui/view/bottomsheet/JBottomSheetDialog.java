bc9d48b6e77ca7814db3ae1e130e2662a2a6f9f1
shiwen.dai <shiwen.dai@qyer.com>
2016-10-22 23:01:17 +0800

    bug: BottomSheetDialog（原生BottomSheetDialog貌似有bug，当拖拽hidden之后再次show没有显示）

--- src/main/java/com/joy/ui/view/bottomsheet/JBottomSheetDialog.java
+++ src/main/java/com/joy/ui/view/bottomsheet/JBottomSheetDialog.java
@@ -6,7 +6,6 @@ import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.StyleRes;
-import android.support.design.widget.BottomSheetDialog;
 import android.view.View;
 import android.view.ViewGroup;
 
