3e957ca3abcf6ef6e4eef08f84f03a6b48639062
shiwen.dai <shiwen.dai@qyer.com>
2017-06-26 17:10:31 +0800

    删除ExLayoutWidget、ExViewWidget，功能由它们的父类ExViewWidget来统一承接

--- src/main/java/com/joy/ui/widget/ExBaseWidget.java
+++ src/main/java/com/joy/ui/widget/ExBaseWidget.java
@@ -2,28 +2,67 @@ package com.joy.ui.widget;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.support.annotation.ColorRes;
+import android.support.annotation.DimenRes;
+import android.support.annotation.IdRes;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.Nullable;
+import android.support.annotation.StringRes;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout.LayoutParams;
+
+import com.joy.utils.DensityUtil;
+import com.joy.utils.LayoutInflater;
+import com.joy.utils.ToastUtil;
+
+import static android.view.ViewGroup.LayoutParams.MATCH_PARENT;
 
 /**
  * 布局的组件类，用于分离Activity中成块的布局
  *
  * @author yhb
+ *         <p>
+ *         Modified by Daisw on 2017/6/25.
  */
 public abstract class ExBaseWidget {
 
     private Activity mActivity;
     private View mContentView;
-    private OnWidgetViewClickListener mClickListener;
+    private OnViewClickListener mOnViewClickListener;
 
     protected ExBaseWidget(Activity activity) {
         mActivity = activity;
     }
 
-    protected void setContentView(View v) {
-        mContentView = v;
+    protected final void setContentView(@LayoutRes int layoutResId) {
+        setContentView(inflateLayout(layoutResId));
+    }
+
+    protected final void setContentView(@LayoutRes int layoutResId, ViewGroup.LayoutParams params) {
+        setContentView(inflateLayout(layoutResId), params);
+    }
+
+    protected final void setContentView(View contentView) {
+        setContentView(contentView, new LayoutParams(MATCH_PARENT, MATCH_PARENT));
+    }
+
+    public final void setContentView(View contentView, ViewGroup.LayoutParams params) {
+        contentView.setLayoutParams(params);
+        mContentView = contentView;
+
+        wrapContentView(contentView);
+
         initData();
         initTitleView();
         initContentView();
+    }
+
+    public void wrapContentView(View contentView) {
+        // add transition animation
+//        LayoutTransition lt = new LayoutTransition();
+//        lt.setDuration(100);
+//        contentParent.setLayoutTransition(lt);
     }
 
     protected void initData() {
@@ -35,11 +74,11 @@ public abstract class ExBaseWidget {
     protected void initContentView() {
     }
 
-    protected boolean isActivityFinishing() {
+    protected final boolean isFinishing() {
         return mActivity == null || mActivity.isFinishing();
     }
 
-    public Activity getActivity() {
+    public final Activity getActivity() {
         return mActivity;
     }
 
@@ -62,17 +101,65 @@ public abstract class ExBaseWidget {
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
     }
 
-    public void setOnWidgetViewClickListener(OnWidgetViewClickListener lisn) {
-        mClickListener = lisn;
+    public void setOnViewClickListener(OnViewClickListener listener) {
+        mOnViewClickListener = listener;
     }
 
-    protected void callbackWidgetViewClickListener(View v) {
-        if (mClickListener != null) {
-            mClickListener.onWidgetViewClick(v);
+    protected void callbackOnViewClickListener(View v) {
+        if (mOnViewClickListener != null) {
+            mOnViewClickListener.onViewClick(v);
         }
     }
 
-    public interface OnWidgetViewClickListener {
-        void onWidgetViewClick(View v);
+    public interface OnViewClickListener {
+        void onViewClick(View v);
+    }
+
+    public <T extends View> T findViewById(@IdRes int id) {
+        return (T) getContentView().findViewById(id);
+    }
+
+    public final void showToast(String text) {
+        ToastUtil.showToast(getActivity().getApplicationContext(), text);
+    }
+
+    public final void showToast(@StringRes int resId) {
+        showToast(getString(resId));
+    }
+
+    public final void showToast(@StringRes int resId, Object... formatArgs) {
+        showToast(getString(resId, formatArgs));
+    }
+
+    public final String getString(@StringRes int resId) {
+        return getActivity().getResources().getString(resId);
+    }
+
+    public final String getString(@StringRes int resId, Object... formatArgs) {
+        return getActivity().getResources().getString(resId, formatArgs);
+    }
+
+    public final <T extends View> T inflateLayout(@LayoutRes int layoutResId) {
+        return LayoutInflater.inflate(getActivity(), layoutResId);
+    }
+
+    public final <T extends View> T inflateLayout(@LayoutRes int layoutResId, @Nullable ViewGroup root) {
+        return LayoutInflater.inflate(getActivity(), layoutResId, root);
+    }
+
+    public final <T extends View> T inflateLayout(@LayoutRes int layoutResId, @Nullable ViewGroup root, boolean attachToRoot) {
+        return LayoutInflater.inflate(getActivity(), layoutResId, root, attachToRoot);
+    }
+
+    public final int DP(float dp) {
+        return DensityUtil.dip2px(getActivity().getApplicationContext(), dp);
+    }
+
+    public final int getDimensionPixelSize(@DimenRes int dimensId) {
+        return DensityUtil.getDimensionPixelSize(getActivity().getApplicationContext(), dimensId);
+    }
+
+    public final int getColorInt(@ColorRes int colorResId) {
+        return getActivity().getResources().getColor(colorResId);
     }
 }
